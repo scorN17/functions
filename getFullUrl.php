136ed0ff101465cd42784cc309882475f9767bc7
Сергей <scorn17@gmail.com>
2016-06-27 12:47:05 +0300

    Create getFullUrl.php

---
+++ getFullUrl.php
@@ -0,0 +1,35 @@
+<?php
+function get_full_url( $url, $webs, $page )
+{
+	$tmp= trim( $url );
+	
+	$page= explode( "?", $page );
+	$page= $page[ 0 ];
+	
+	$arr= explode( "/", page_from_url( $page ) );
+	unset( $arr[ count( $arr ) - 1 ] );
+	unset( $arr[ 0 ] );
+	$dirname= '';
+	foreach( $arr AS $val )
+	{
+		$dirname .= "/". $val;
+	}
+	$dirname .= "/";
+	
+	if( substr( $tmp, 0, 7 ) == 'mailto:' ) $tmp= 'http://' . $webs . '/';
+	elseif( substr( $tmp, 0, 11 ) == 'javascript:' ) $tmp= 'http://' . $webs . '/';
+	elseif( substr( $tmp, 0, 2 ) == '//' ) $tmp= 'http:' . $tmp;
+	elseif( substr( $tmp, 0, 1 ) == '/' ) $tmp= 'http://' . $webs . get_url_bez_tochek( $tmp );
+	elseif( substr( $tmp, 0, 7 ) == 'http://' ) NULL;
+	elseif( substr( $tmp, 0, 8 ) == 'https://' ) NULL;
+	elseif( substr( $tmp, 0, 1 ) == '?' ) $tmp= $page . $url;
+	else $tmp= 'http://'. $webs . get_url_bez_tochek( ( $dirname != 'http:/' ? $dirname : '' ) . $tmp );
+	
+	$tmp= explode( "#", $tmp );
+	$tmp= $tmp[ 0 ];
+	
+	$tmp= str_replace( "www.", '', $tmp );
+	
+	return $tmp;
+}
+?>
