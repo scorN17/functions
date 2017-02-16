60b03145d23e463be27e12b224c2e893f091b212
Сергей <scorn17@gmail.com>
2017-02-16 13:54:03 +0300

    Create curl_exec_followlocation.php

---
+++ curl_exec_followlocation.php
@@ -0,0 +1,31 @@
+
+function curl_exec_followlocation(&$curl, &$uri)
+{
+	// v2.0
+	// Date 13.01.2017
+	// -----------------------------------------
+	if(preg_match("/^(http(s){0,1}:\/\/[a-z0-9\.-]+)(.*)$/i", $uri, $matches)!==1) return;
+	$website= $matches[1];
+	do{
+		// if($referer) curl_setopt($curl, CURLOPT_REFERER, $referer);
+		curl_setopt($curl, CURLOPT_URL, $uri);
+		$response= curl_exec($curl);
+		if(curl_errno($curl)) return false;
+		$headers= str_replace("\r",'',$response);
+		$headers= explode("\n\n",$headers,2);
+		if(preg_match("/^location: (.*)$/im", $headers[0], $matches)===1)
+		{
+			$location= true;
+			$referer= $uri;
+			$uri= trim($matches[1]);
+			if(preg_match("/^http(s){0,1}:\/\/[a-z0-9\.-]+/i", $uri, $matches)!==1)
+				$uri= $website.(substr($uri,0,1)!='/'?'/':'').$uri;
+		}else $location= false;
+		if($location)
+		{
+			if($redirects_list[$uri]<=1) $redirects_list[$uri]++;
+				else $location= false;
+		}
+	}while($location);
+	return $response;
+}
